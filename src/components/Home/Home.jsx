cc218cd36e3e182ff2fe26890233b893899b981c
safvanms <muhammedsafvan1ms@gmail.com>
2023-06-08 21:15:43 +0530

    bug fixing

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom'
 import Header from '../Other/Header'
 import Footer from '../Other/Footer'
 
-
 export default function Home() {
   const [surahNumber, setSurahNumber] = useState([])
   const [surahNames, setSurahNames] = useState([])
@@ -14,9 +13,7 @@ export default function Home() {
   useEffect(() => {
     const fetchAllSurah = async () => {
       try {
-        const response = await axios.get(
-          'http://localhost:5000/quran',
-        )
+        const response = await axios.get('http://localhost:5000/quran')
         const surahs = response.data.data.surahs
 
         const surahNumbers = surahs.map((surah) => surah.number)
@@ -34,26 +31,64 @@ export default function Home() {
     fetchAllSurah()
   }, [])
 
+  const data = [
+    {
+      id: 1,
+      name: 'sss',
+    },
+    {
+      id: 2,
+      name: 'nnnn',
+    },
+    {
+      id: 3,
+      name: 'kkkk',
+    },
+    {
+      id: 4,
+      name: 'okol',
+    },
+  ]
 
   return (
     <>
       <Header />
       <div className="home-container">
         <div className="home-sec">
-          {surahNumber?surahNumber.map((number, index) => (
+          {/* {surahNumber?.map((number, index) => (
             <Link
               style={{ color: 'black', textDecoration: 'none' }}
               to={`/${number}`}
             >
-              <div key={number} className="surah-list">
+              {console.log(index)}
+              <div key={index} className="surah-list">
                 <span>{number}</span>
-                <span style={{fontSize:"12px",color:"grey"}}>{englishName[index]}</span>
+                <span style={{ fontSize: '12px', color: 'grey' }}>
+                  {englishName[index]}
+                </span>
                 <span className="surah-name">
                   <span>{surahNames[index]}</span>
                 </span>
               </div>
             </Link>
-          )):"No Surah found !"}
+          ))} */}
+          {data?.map((da, index) => (
+            <Link
+              style={{ color: 'black', textDecoration: 'none' }}
+              // to={`/${number}`}
+            >
+              {console.log(index)}
+              <div key={index} className="surah-list">
+                <span>{da.id}</span>
+                <span style={{ fontSize: '12px', color: 'grey' }}>
+                  {da.name}
+                </span>
+                <span className="surah-name">
+                  {/* <span>{surahNames[index]}</span> */}
+                </span>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
       <Footer />
